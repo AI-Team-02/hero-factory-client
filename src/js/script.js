c974259ca65565f051840dd1a83c03a39f9032b7
kgj <nino3912@gmail.com>
2024-10-18 10:58:58 +0900

    update: 반응형웹 오류수정 및 스파게티 코드 수정

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+function func1() {
+
+    let ref = document.querySelector("div#ref").innerText;
+    document.querySelector("#output").innerText = ref;
+
+}
+
+function func2() {
+
+    document.querySelector("#output").style.fontSize = "30px";
+    document.querySelector("#output").style.fontWeight = "bold";
+    document.querySelector("#output").style.border = "4px solid #f80";
+
+}
